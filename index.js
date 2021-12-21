8b01dab0c627572c43ac933c15da50281961d227
claudia welch <claudia.welch73@gmail.com>
2021-12-21 12:12:18 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+
+const cats=["Milo", "Otis", "Garfield"];
+
+
+    function destructivelyAppendCat(name){
+    cats.splice(3,0,name);
+
+    }
+    //destructivelyAppendCat('Ralph');
+
+
+    function destructivelyPrependCat(name){
+    cats.unshift(name);
+    
+    }
+    //destructivelyPrependCat('Bob');
+
+    function destructivelyRemoveLastCat(){
+    cats.pop();
+   
+    }
+    function destructivelyRemoveFirstCat(){
+    cats.shift()
+
+    }
+
+    function appendCat(name){
+    const newCats=[...cats,name];
+    return newCats;
+    
+    }
+    //appendCat('Broome');
+   
+    function prependCat(name){
+        const fstCat=[name, ...cats];
+        return fstCat;
+    }
+    //prependCat('Arnold')
+    
+  function removeLastCat(){
+      const lstCat=[...cats.slice(0,cats.length -1)];
+      console.log(lstCat);
+      return lstCat;
+  }
+  
+  function removeFirstCat(){
+      const removeFirst=[...cats.slice(1)];
+      return removeFirst;
+  }
